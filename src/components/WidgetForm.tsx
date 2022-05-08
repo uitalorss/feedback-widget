af0e173b792a6b684fc043611309b95d9b379e08
Uítalo <uitalo.urss@gmail.com>
2022-05-07 21:31:40 -0300

    colocando ação nos botoes de opções de feedback(inicio)

--- src/components/WidgetForm.tsx
+++ src/components/WidgetForm.tsx
@@ -3,6 +3,7 @@ import { CloseButton } from "./CloseButton";
 import bugImageUrl from '../assets/emoji-bug.svg';
 import ideaImageUrl from '../assets/emoji-idea.svg';
 import otherImageUrl from '../assets/emoji-other.svg';
+import { useState } from "react";
 
 const feedbackTypes = {
   BUG: {
@@ -28,7 +29,11 @@ const feedbackTypes = {
   }
 }
 
+type feedbackType = keyof typeof feedbackTypes
+
 export function WidgetForm(){
+  const [feedbackType, setFeedbackType] = useState<feedbackType | null>(null)
+
   return(
     <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
       <header>
@@ -39,7 +44,9 @@ export function WidgetForm(){
         { Object.entries(feedbackTypes).map(([key, value]) => {
             return (
               <button
+                key={key}
                 className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500"
+                onClick={() => setFeedbackType(key as feedbackType)}
               >
                 <img src={value.image.source} alt={value.image.alt} />
                 <span>{value.title}</span>
