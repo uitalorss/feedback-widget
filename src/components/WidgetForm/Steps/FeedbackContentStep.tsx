a3ef9c025346701c8450f09786a8977631a29e18
Uítalo <uitalo.urss@gmail.com>
2022-05-08 16:15:50 -0300

    criando a tela de envio do feedback

--- src/components/WidgetForm/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetForm/Steps/FeedbackContentStep.tsx
@@ -27,9 +27,19 @@ export function FeedbackContentStep(props: FeedbackContentProps){
       </span>
       <CloseButton />
     </header>
-    <div className="flex py-8 gap-2">
-    <h1>Você conseguiu selecionar um item.</h1>
-    </div>
+    <form className="my-4 w-full">
+      <textarea 
+        className="min-w-[304px] w-full min-h-[112px] text-sm placeholder-zinc-400 text-zinc-100 border-zinc-600 bg-transparent rounded-md"
+        placeholder="Conte com detalhes o que está acontecendo..."
+      />
+
+      <footer className="flex gap-2 mt-2">
+        <button type="submit" 
+        className="p-2 bg-brand-500 border-transparent flex-1 flex justify-center items-center text-sm rounded-md hover:bg-brand-300 transition-colors">
+          Enviar Feedback
+        </button>
+      </footer>
+    </form>
     </>
   )
 }
